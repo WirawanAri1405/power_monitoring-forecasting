f90b8d38c4935f06e053ea0b9e08bef970e9db45
WirawanAri1405 <wirawanariyoso123@gmail.com>
2025-09-24 20:28:03 +0700

    add tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,6 +28,23 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+       <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Hello Tailwind + React + TS 🚀
+      </h1>
+      <p className="text-lg mb-6">
+        Kalau kamu lihat teks ini berwarna biru dan layout rapi, berarti Tailwind sudah jalan.
+      </p>
+
+      <div className="flex gap-4">
+        <button className="px-4 py-2 rounded-lg font-semibold text-white bg-blue-600 hover:bg-blue-700">
+          Primary
+        </button>
+        <button className="px-4 py-2 rounded-lg font-semibold text-gray-700 bg-gray-200 hover:bg-gray-300">
+          Secondary
+        </button>
+      </div>
+    </div>
     </>
   )
 }
